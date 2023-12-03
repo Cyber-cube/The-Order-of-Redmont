d2f66337f050c4abf42d68b66a68752f2b9e2286
Cyber-cube <112477661+Cyber-cube@users.noreply.github.com>
2023-12-03 16:23:56 +0000

    Change

--- static/script.js
+++ static/script.js
@@ -1,23 +1,16 @@
-
 window.onload = function() {
   var chance = new Chance();
-
   var id = localStorage.getItem('id');
-
   if (id === null) {
     id = generateId();
     localStorage.setItem('id', id);
-    document.getElementById('id').value = id;
-    document.getElementById('id2').value = id;
-    
+    document.querySelector('.id') = id;
   } else {
-     document.getElementById('id').value = id;
-    document.getElementById('id2').value = id;
-  }
-
+    document.querySelector('.id') = id;
+}
   function generateId() {
     var newId = chance.string({ length: 16, pool: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' });
-     document.getElementById('id').value = newId;
+     document.querySelector('.id') = newId;
     return newId;
   }
 };
